d7b155db6e1626b2581723fd264e5445ab13b882
midoshehab335@gmail.com <MAImido123>
2023-10-23 22:27:28 +0300

    update

--- liquibase-demo/src/main/java/com/example/liquibasedemo/model/Person.java
+++ liquibase-demo/src/main/java/com/example/liquibasedemo/model/Person.java
@@ -13,7 +13,7 @@ public class Person {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "firstname")
+    @Column(name = "firstname1111")
     private String firstName;
 
     @Column(name = "lastname", nullable = false)
