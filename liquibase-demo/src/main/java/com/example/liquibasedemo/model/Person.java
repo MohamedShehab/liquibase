0f4f39df16e775fadcce96c9e5061ef9d3022987
midoshehab335@gmail.com <MAImido123>
2023-10-23 22:08:07 +0300

    update

--- liquibase-demo/src/main/java/com/example/liquibasedemo/model/Person.java
+++ liquibase-demo/src/main/java/com/example/liquibasedemo/model/Person.java
@@ -1,11 +1,14 @@
 package com.example.liquibasedemo.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 
 @Entity
 @Table(name = "persons")
-@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
 
     @Id
